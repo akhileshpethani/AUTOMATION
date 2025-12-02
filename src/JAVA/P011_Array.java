c95f2fd198713ada5a2561093cc1934fad92e116
akhileshpethani <akhileshpethani@gmail.com>
2025-12-02 15:30:06 +0530

    AUTOMATION

--- src/JAVA/P011_Array.java
+++ src/JAVA/P011_Array.java
@@ -1,6 +1,6 @@
 package JAVA;
 
-
+import java.util.Scanner;
 
 public class P011_Array {
 	public static void main(String[] args) {
@@ -12,6 +12,36 @@ public class P011_Array {
 			System.out.println(arr[index]);
 		}
 		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("enter size of array : ");
+		int size = sc.nextInt();
+		int ar[] = new int[size];
+		for (int index = 0; index < ar.length; index++) {
+			System.out.print("enter element at ar[" + index + "] : ");
+			ar[index] = sc.nextInt();
+		}
+		int sum = 0;
+		for (int index = 0; index < ar.length; index++) {
+			sum = sum + ar[index];
+			System.out.println("sum : " + sum);
+		}
+		System.out.println("sum : " + sum);
+
+		System.out.println("enter num to search in array : ");
+		int num = sc.nextInt();
+
+		int counter = 0;
+		for (int index = 0; index < ar.length; index++) {
+			if (num == ar[index]) {
+				counter++;
+			}
+		}
+		if (counter > 0) {
+			System.out.println("yes exist " + counter + " times");
+		} else {
+			System.out.println("not exist");
+		}
 		
 	
 		}
